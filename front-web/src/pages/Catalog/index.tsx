6ea34708d3cdcaa83849f7f852b9c6d594871ddb
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2021-02-09 21:06:35 -0300

    starting backend integration

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
-const Catalog  = () => (
-    <div className="catalog-container">
+const Catalog  = () => {
+    //quando o componente iniciar, buscar a lista de produtos
+
+    //quando a lista de produtos estiver disponível, popular um esto no componente e listar os produtos dinamicamente
+
+    useEffect(() => {
+        fetch('http://localhost:3000/products')
+        .then(response => response.json())
+        .then(response => console.log(response))
+
+    },[]);
+
+
+    return (
+        <div className="catalog-container">
         <h1 className="catalog-title">
             Catálogo de Produtos
         </h1>
@@ -20,6 +33,7 @@ const Catalog  = () => (
             <Link to="/products/9"><ProductCard /></Link>
         </div>
     </div>
-);
+    );
+}
 
 export default Catalog;
