e8ce10a5d80a1c5e2079740a08d9b2455de8ced7
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2021-02-15 15:18:57 -0300

    Pagination finished

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -3,7 +3,6 @@ import {ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg'
 import './styles.scss'
 import { generateList } from 'core/utils/list';
 
-
 type Props = {
     totalPages: number;
     activePage: number;
@@ -11,13 +10,17 @@ type Props = {
 }
 
 const Pagination = ({totalPages,activePage, onChange}: Props) => {
-
     const items = generateList(totalPages);
+    const previousClass = totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
+    const nextClass = (activePage + 1) < totalPages ? 'page-active' : 'page-inactive';
 
     
     return (
         <div className="pagination-container">
-            <ArrowIcon className="pagination-previous"/>
+            <ArrowIcon
+                className={`pagination-previous ${previousClass}`}
+                onClick={()=> onChange(activePage - 1)}
+            />
             {items.map(item => (
                 <div
                     key={item}
@@ -27,11 +30,12 @@ const Pagination = ({totalPages,activePage, onChange}: Props) => {
                     {item + 1}
                 </div>
             ))}
-            <ArrowIcon className="pagination-next" />
+            <ArrowIcon
+                className={`pagination-next ${nextClass}`}
+                onClick={()=> onChange(activePage + 1)}
+            />
         </div>
-
-    )
-
+    );
 }
 
 export default Pagination;
