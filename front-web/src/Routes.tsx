cc1c858912d220dbab33dfd020d95c12c0625821
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2021-02-16 14:30:28 -0300

    Admin routes

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
@@ -9,7 +9,7 @@ import Home from './pages/Home';
 
 const Routes = () => (
     <BrowserRouter>
-    <Navbar/>
+        <Navbar/>
         <Switch>
             <Route path="/" exact>
                 <Home/>
@@ -21,6 +21,7 @@ const Routes = () => (
                 <ProductDetails/>
             </Route>
             <Route path="/admin">
+                <Redirect to="/admin/products"/>
                 <Admin/>
             </Route>
         </Switch>
